ad02b6a134caae3fb48606f0a7a1d2cd3fd5e7d0
MD_ganlirong <122678672@qq.com>
2015-05-26 17:29:26 +0800

    第一次导入 甘立荣 2015年05月26日17:28:48

---
+++ LRBannerView/LRBannerView/LRBannerView.h
@@ -0,0 +1,13 @@
+//
+//  LRBannerView.h
+//  LRBannerView
+//
+//  Created by 甘立荣 on 15/5/26.
+//  Copyright (c) 2015年 甘立荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRBannerView : UIView
+
+@end
---
+++ LRBannerView/LRBannerView/LRBannerView.m
@@ -0,0 +1,21 @@
+//
+//  LRBannerView.m
+//  LRBannerView
+//
+//  Created by 甘立荣 on 15/5/26.
+//  Copyright (c) 2015年 甘立荣. All rights reserved.
+//
+
+#import "LRBannerView.h"
+
+@implementation LRBannerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
